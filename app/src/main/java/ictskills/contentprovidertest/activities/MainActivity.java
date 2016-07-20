24bf040d6511d64680a93d5ff75a931a57211392
John Fitzgerald <usplitu@hotmail>
2016-07-20 12:42:17 +0100

    remove dead code

--- app/src/main/java/ictskills/contentprovidertest/activities/MainActivity.java
+++ app/src/main/java/ictskills/contentprovidertest/activities/MainActivity.java
@@ -1,31 +1,29 @@
 package ictskills.contentprovidertest.activities;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
-
 import ictskills.contentprovidertest.R;
 import ictskills.contentprovidertest.models.Residence;
 import ictskills.contentprovidertest.providers.ResidenceContract;
 
 import android.widget.Toast;
 
-
+/**
+ * Set a button click handler.
+ * Handler obtains list Residence records from MyRentSqlite
+ * Precondition: MyRentSqlite database must be present.
+ * Install and run MyRentSqlite and add some Residence records to populate the database.
+ *
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-  private static  final int     REQUEST_RESIDENCE = 1;
-
   private Button contentProvider;
 
   @Override
@@ -37,6 +35,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     contentProvider.setOnClickListener(this);
   }
 
+  @Override
+  public void onClick(View v) {
+    switch(v.getId()) {
+      case R.id.contentProvider:
+        selectResidences();
+    }
+  }
+
+  /**
+   * This code derived from MyRentSQLite.RefreshResidenceService.selectResidences.
+   */
   private void selectResidences() {
 
     // Query the database
@@ -62,57 +71,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
       } while (cursor.moveToNext());
     }
     cursor.close();
-
+    Toast.makeText(this, "Retrieved MyRentSQLite Residence array, size: " + residences.size(), Toast.LENGTH_SHORT).show();
   }
-
-  @Override
-  public void onClick(View v) {
-    switch(v.getId()) {
-      case R.id.contentProvider:
-        //Intent intent = new Intent(Intent.ACTION_PICK, ResidenceContract.CONTENT_URI);
-        //startActivityForResult(intent, REQUEST_RESIDENCE);
-        Toast.makeText(this, "MainActivity.onClick invoked for ContentProvider", Toast.LENGTH_SHORT).show();
-        selectResidences();
-    }
-  }
-
-//  @Override
-//  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//    if (resultCode != Activity.RESULT_OK) {
-//      return;
-//    }
-//    switch (requestCode) {
-//      case REQUEST_RESIDENCE:
-//        selectResidences(this, data);
-//        break;
-//    }
-//  }
-
-//    public void selectResidences(Context context, Intent data)
-//    {
-//      // Query the database
-//      List<Residence> residences = new ArrayList<Residence>();
-//      Cursor cursor = getContentResolver().query(ResidenceContract.CONTENT_URI, null, null, null, null);
-//
-//      if (cursor.moveToFirst()) {
-//        int columnIndex = 1; // skip column 0, the _id
-//        do {
-//          Residence residence = new Residence();
-//
-//          residence.uuid = UUID.fromString(cursor.getString(columnIndex++));
-//          residence.geolocation = cursor.getString(columnIndex++);
-//          residence.date = new Date(Long.parseLong(cursor.getString(columnIndex++)));
-//          residence.rented = cursor.getString(columnIndex++) == "yes" ? true : false;
-//          residence.tenant = cursor.getString(columnIndex++);
-//          residence.zoom = Double.parseDouble(cursor.getString(columnIndex++));
-//          residence.photo = cursor.getString(columnIndex++);
-//
-//          columnIndex = 1;
-//
-//          residences.add(residence);
-//        } while (cursor.moveToNext());
-//      }
-//      cursor.close();
-//
-//    }
 }
